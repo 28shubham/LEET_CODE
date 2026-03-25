e24d931b7dd304bd8886d1397249f94b6514ebca
SHUBHAM THAWAIT <71043474+28shubham@users.noreply.github.com>
2026-03-25 23:51:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -11,14 +11,14 @@ public:
         return mid;
        }
        else if(nums[low]<=nums[mid]){
-        if(nums[low]<=target && target <= nums[mid]){
+        if(nums[low]<=target && target <nums[mid]){
             high = mid -1;
         }
         else {
             low = mid+1;
         }
        } else{
-        if(nums[mid]<=target && target <=nums[high]){
+        if(nums[mid]<target && target <=nums[high]){
             low = mid+1;
         }
         else{
