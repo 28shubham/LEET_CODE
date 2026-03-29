1da50fd901f42a2c769a217663015fca054d58b9
SHUBHAM THAWAIT <71043474+28shubham@users.noreply.github.com>
2026-03-29 20:10:37 +0530

    Time: 0 ms (100%), Space: 23.1 MB (12.38%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -2,45 +2,24 @@ class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int maxPile = *max_element(piles.begin(), piles.end());
-         int minPile = 0; //*min_element(piles.begin(), piles.end());
+         int minPile = 1; //*min_element(piles.begin(), piles.end());
          int mid;
          int size = piles.size();
        
-         int count = 0 ;
+         long long count = 0;
          int ans=0;
-         if(size==1){
-            int l = piles[0]/h;
-            if(piles[0]%h == 0){
-                return l;
-            }
-            else{
-                return l+1;
-            }
-         }
+        
          while(minPile <= maxPile){
             mid = minPile + (maxPile- minPile)/2;
             count = 0 ;
             for(int i=0 ; i <size;i++){
-                if( piles[i]>mid ){
-                     if(mid < 1){
-                    return ans;
-                }
-                    int k = piles[i]/mid;
-                    count = count + k;
-                    if(piles[i]%mid == 0){
-                        count--;
-                    }
-                } 
+               count = ceil((double)piles[i]/mid)+count ; 
             }
-            count = count + size;
-            cout<<count<<" ";
-            cout<<" "<<mid<<" ";
+           
             if (count <= h){
                 
                 ans = mid;
-                if(mid < 1){
-                    return ans;
-                }
+               
                 maxPile = mid -1;
             }
             else {
